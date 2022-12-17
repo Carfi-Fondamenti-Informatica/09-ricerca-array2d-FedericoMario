2b8e90c1fb744500ee27d06a97cc10ca5022e55e
FedericoMario <117830052+FedericoMario@users.noreply.github.com>
2022-12-17 10:52:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 
 int main(){
-  return 0;
+  char a[10][20];
+    char b[1][20];
+    for(int i=0; i<10; i++) {
+        for (int j = 0; j < 20; j++) {
+            a[i][j] = ' ';
+        }
+        cin >> a[i];
+    }
+    for(int j=0; j<20; j++){
+        b[0][j] = ' ';
+    }
+    cin>>b[0];
+    if(nomi(a,b)==-1){
+        cout<<"non presente";
+    }else{
+        cout<<nomi(a,b);
+    }
+    return 0;
 }
