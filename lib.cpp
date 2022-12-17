f9a2ebb22fc77d9e47630b12d188dfe83b4468b2
FedericoMario <117830052+FedericoMario@users.noreply.github.com>
2022-12-17 10:50:47 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+int nomi(char a[][20], char b[1][20]) {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 20; j++) {
 
+            if (a[i][j] != b[0][j]) {
+                break;
+            }
+            if (j == 19) {
+                return i;
+            }
+        }
+    if (i == 9) {
+        return -1;
+    }
+}
+}
