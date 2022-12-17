94dc9ec1a835105ed65f4de2833cd1aa07dcf2dc
FedericoMario <117830052+FedericoMario@users.noreply.github.com>
2022-12-17 11:00:53 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,18 +1,14 @@
 #include "lib.h"
-
-int nomi(char a[][20], char b[1][20]) {
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 20; j++) {
-
-            if (a[i][j] != b[0][j]) {
-                break;
-            }
-            if (j == 19) {
-                return i;
-            }
+bool controllo(char nomi[10][20], char nome[20], int &c){
+    for (int n = 0; n < 10; n++){
+    for (int i = 0; i < 20; i++)
+        {
+            if (nomi [n][i] != nome[i]){
+                i = 20;  }
+            if (i == 19){
+                c = n;
+                return true;}
         }
-    if (i == 9) {
-        return -1;
     }
-}
+    return false;
 }
